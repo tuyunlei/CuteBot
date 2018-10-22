c648b7d890eccf448a69f088901fc94c931426f1
tuyunlei <1206294056@qq.com>
2018-10-22 13:27:06 +0800

    first attempt

---
+++ app/src/main/java/com/xclz/lhz/cutebot/FloatObject.java
@@ -0,0 +1,12 @@
+package com.xclz.lhz.cutebot;
+
+import android.app.Service;
+
+public class FloatObject {
+
+	private Service mService;
+	
+	public FloatObject(Service service) {
+		mService = service;
+	}
+}
