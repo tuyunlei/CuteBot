af601a6e7e5c12c3bf5f9f410667318e78e9d839
tuyunlei <1206294056@qq.com>
2018-10-23 13:38:59 +0800

    change framework

---
+++ app/src/main/java/com/xclz/lhz/cutebot/FloatWindow.java
@@ -0,0 +1,63 @@
+package com.xclz.lhz.cutebot;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+
+public class FloatWindow {
+	public final static int NORMAL_WIDTH = 100;
+	public final static int NORMAL_HEIGHT = 100;
+
+	private boolean isActivity;
+
+	private View mView;
+	private Context mContext;
+	private WindowManager mWindowManager;
+	private WindowManager.LayoutParams mParams;
+	
+
+	public FloatWindow(Service service, View view) {
+		mView = view;
+		mContext = service;
+		isActivity = false;
+	}
+	
+	public FloatWindow(Activity activity, View view) {
+		mView = view;
+		mContext = activity;
+		isActivity = true;
+	}
+	
+	public WindowManager.LayoutParams getParam() {
+		if(mParams == null) {
+			mParams = new WindowManager.LayoutParams();
+			mParams.type = mParams.TYPE_SYSTEM_ALERT;
+			mParams.flags = mParams.FLAG_NOT_FOCUSABLE | mParams.FLAG_SHOW_WHEN_LOCKED;
+			mParams.gravity = Gravity.RIGHT | Gravity.CENTER_VERTICAL;
+			mParams.width = NORMAL_WIDTH;
+			mParams.height = NORMAL_HEIGHT;
+			mParams.format = PixelFormat.RGBA_8888;
+		}
+		return mParams;
+	}
+	
+	public void create() {
+		this.create(getParam());
+	}
+	
+	public void create(WindowManager.LayoutParams param) {
+		mParams = param;
+		mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+		mWindowManager.addView(mView, param);
+	}
+	
+	public void destroy() {
+		mWindowManager.removeView(mView);
+		mView = null;
+	}
+}
